3d13260360feca171d3f333e55a0bf42199b100b
HRazim <rhaziim78@gmail.com>
2024-10-16 17:34:49 +0200

    Modification du titre et ajout du logo et d'une description pour la page d'accueil

--- src/site/pages/accueil.php
+++ src/site/pages/accueil.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil - Mon site web</title>
+    <title>Proβability - Accueil</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
@@ -11,11 +11,14 @@
 <?php include '../includes/header.php'; ?>
 
 <div class="container">
+    <div class="logo-container">
+        <img src="../images/logo.png" alt="Logo Proβability">
+
+    </div>
     <div class="content">
         <h2>Bienvenue sur notre site !</h2>
-        <p>Voici mon site web ou vous avez la possibilité de
-        personaliser votre Damier. Vous disposez de la possibilité de voir
-            le Nom,Prénom,Groupe,Calcul Numérique,Date et heure des étudiants</p>
+        <p>Ce site vous permet d'accéder à différents
+            modules pour effectuer des calculs complexes.</p>
     </div>
 </div>
 
