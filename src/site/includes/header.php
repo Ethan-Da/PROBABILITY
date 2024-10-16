35c5b2c671199cf8bd1b0362153f908b23c42c7d
HRazim <rhaziim78@gmail.com>
2024-10-16 18:35:08 +0200

    Implémentation du header et renommage des fichiers damier et note

--- src/site/includes/header.php
+++ src/site/includes/header.php
@@ -1,10 +1,26 @@
-<header>
-    <nav>
-        <ul style="list-style-type: none; padding: 0; display: flex; justify-content: center;">
-            <li style="margin: 0 15px;"><a href="accueil.php" style="color: white; text-decoration: none;">Accueil</a></li>
-            <li style="margin: 0 15px;"><a href="damier.php" style="color: white; text-decoration: none;">Damier</a></li>
-            <li style="margin: 0 15px;"><a href="notes.php" style="color: white; text-decoration: none;">Tableau de notes</a></li>
-            <li style="margin: 0 15px;"><a href="Formulaire.php" style="color: white; text-decoration: none;">Filtres</a></li>
-        </ul>
-    </nav>
+<header style="background-color: #666666; padding: 10px 20px; display: flex; align-items: center; justify-content: space-between;">
+    <div style="display: flex; align-items: center; justify-content: center; width: 100%;">
+        <a href="accueil.php">
+        <img src="../images/logo2.png" alt="Logo" style="height: 40px; margin-right: 20px;">
+        </a>
+        <nav>
+            <ul style="list-style-type: none; padding: 0; display: flex;">
+                <li style="margin-right: 20px;"><a href="accueil.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active')" onmouseout="this.classList.remove('active')">Accueil</a></li>
+                <li style="margin-right: 20px;"><a href="module1.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active')" onmouseout="this.classList.remove('active')">Module1</a></li>
+                <li style="margin-right: 20px;"><a href="module2.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active')" onmouseout="this.classList.remove('active')">Module2</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div style="background-color: white; height: 40px; width: 40px; border-radius: 50%;">
+        <a href="Formulaire.php" style="display: block; width: 100%; height: 100%;"></a>
+    </div>
 </header>
+
+<style>
+    a.active {
+        color: #000;
+        background-color: #b0d8f8;
+        padding: 5px 10px;
+        border-radius: 5px;
+    }
+</style>
