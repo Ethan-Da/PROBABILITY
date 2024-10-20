ef14ef525fda59fc11f4e970cfe7a1c9f0804912
ArseneMax <arsenemaxencezs@gmail.com>
2024-10-20 15:10:38 +0200

    changement du header avec le replacement des modules et l'attribution de leur couleurs (fait comme l'exemple de la charte graphique) (+ajout d'un fichier module3 et module4

--- src/site/includes/header.php
+++ src/site/includes/header.php
@@ -1,13 +1,15 @@
-<header style="background-color: #666666; padding: 10px 20px; display: flex; align-items: center; justify-content: space-between;">
-    <div style="display: flex; align-items: center; justify-content: center; width: 100%;">
+<header style="background-color: #666666; padding: 10px 20px; display: flex; align-items: center;">
+    <div style="display: flex; align-items: center; justify-content: flex-start; width: 100%;">
         <a href="index.php">
         <img src="../images/logo2.png" alt="Logo" style="height: 80px; margin-right: 20px;">
         </a>
         <nav>
             <ul style="list-style-type: none; padding: 0; display: flex;">
                 <li style="margin-right: 20px;"><a href="index.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active')" onmouseout="this.classList.remove('active')">Accueil</a></li>
-                <li style="margin-right: 20px;"><a href="module1.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active')" onmouseout="this.classList.remove('active')">Module1</a></li>
-                <li style="margin-right: 20px;"><a href="module2.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active')" onmouseout="this.classList.remove('active')">Module2</a></li>
+                <li style="margin-right: 20px;"><a href="module1.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active1')" onmouseout="this.classList.remove('active1')">Module1</a></li>
+                <li style="margin-right: 20px;"><a href="module2.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active2')" onmouseout="this.classList.remove('active2')">Module2</a></li>
+                <li style="margin-right: 20px;"><a href="module3.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active3')" onmouseout="this.classList.remove('active3')">Module3</a></li>
+                <li style="margin-right: 20px;"><a href="module4.php" style="color: white; text-decoration: none;" onmouseover="this.classList.add('active4')" onmouseout="this.classList.remove('active4')">Module4</a></li>
             </ul>
         </nav>
     </div>
@@ -20,6 +22,39 @@
     a.active {
         color: #000;
         background-color: #b0d8f8;
+        padding: 5px 10px;
+        border-radius: 5px;
+    }
+
+    a.active1 {
+        color: #000;
+        background-color: #a8d8e8;
+        border: solid #5baccd;
+        border-radius: 5px;
+        padding: 5px 10px;
+        
+    }
+
+    a.active2 {
+        color: #000;
+        background-color: #d1e8bb;
+        border: solid #8d9f7c;
+        padding: 5px 10px;
+        border-radius: 5px;
+    }
+
+    a.active3 {
+        color: #000;
+        background-color: #f1e7e7;
+        border: solid #e89393;
+        padding: 5px 10px;
+        border-radius: 5px;
+    }
+
+    a.active4 {
+        color: #000;
+        background-color: #fffce5;
+        border: solid #c6aa54;
         padding: 5px 10px;
         border-radius: 5px;
     }
