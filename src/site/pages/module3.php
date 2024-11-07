302044d2a4662fb8a9409579a2062f58b03fc551
Ed <ethan.dangeul@gmail.com>
2024-11-07 14:01:50 +0100

    Formulaire module de calcul

--- src/site/pages/module3.php
+++ src/site/pages/module3.php
@@ -11,6 +11,24 @@
 
 <div class="container">
     <h1>Module 3</h1>
+    <div class="content">
+        <form action="Calcul.php" METHOD="post" class="form-damier">
+            <label for="E"> Espérance : </label>
+            <input type="number" name="E" id="E">
+            <label for="F"> Forme : </label>
+            <input type="number" name="F" id="F">
+            <label for="T"> t tel que P(X<\t) : </label>
+            <input type="number" name="T" id="T">
+            <label for="N"> N : </label>
+            <input type="number" name="N" id="N">
+            <label for="Methode"> Méthode de calcul : </label>
+            <input type="text" name="Methode" id="Methode">
+            <input type="submit" name="OK" id="OK" value="Valider">
+        </form>
+    </div>
+
+
+
 </div>
 
 <?php include '../includes/footer.php'; ?>
