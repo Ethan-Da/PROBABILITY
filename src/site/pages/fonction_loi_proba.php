32e84e7d72375c00db3d8a408add9219f40edf0f
Romeo Leblond <22301119@me.uvsq.fr>
2024-11-20 16:33:16 +0100

    Ajout de commentaires

--- src/site/pages/fonction_loi_proba.php
+++ src/site/pages/fonction_loi_proba.php
@@ -1,4 +1,5 @@
 <?php
+################################## Fonction Inverse de Gauss
     function inv_gauss($esp, $lambda, $x) {
         if ($x <= 0) {
         return 0;
@@ -6,23 +7,27 @@
         return sqrt($lambda / (2 * pi() * pow($x, 3))) * exp(-$lambda * pow($x - $esp, 2) / (2 * pow($esp, 2) * $x));
         }
 
+################################## Méthodes de calcul d'intégrales
+
     function  rectangles_gauche($esp, $lambda, $n, $b) {
-        $intervalle = ($b / $n);
+        $proba_fin = [];
+        $intervalle = ($b / $n); //pas besoin de définir a car on commence toujours à 0
         $h = $b / $n;
         $proba = 0;
         for ($i = 1; $i <= $n; $i++) { // Commence à 1 pour éviter la division par zéro
             $x = $intervalle * $i;
             $proba += $intervalle * inv_gauss($esp, $lambda, $x);
+            $proba_fin = [$proba]; //On ajoute les valeurs dans une liste pour pouvoir les afficher plus tard
         }
 
-        return $proba;
+        return $proba_fin;
     }
 
     function  rectangles_median($esp, $lambda, $n, $b) {
         $intervalle = $b / $n;
         $proba = 0;
         for ($i = 0; $i < $n; $i++) {
-            $x = ($i + 0.5) * $$intervalle;
+            $x = ($i + 0.5) * $intervalle;
             $proba += $intervalle * inv_gauss($esp, $lambda, $x);
         }
         return $proba;
