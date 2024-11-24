1bff5c2aaa173a844371241fd18c76ad51ef9c58
Romeolbl <leblondromeo@gmail.com>
2024-11-24 15:59:51 +0100

    Modificatio du return pour pouvoir récuperer les valers de la fonction de densité et le résultat

--- src/site/pages/fonction_loi_proba.php
+++ src/site/pages/fonction_loi_proba.php
@@ -1,49 +1,76 @@
 <?php
 ################################## Fonction Inverse de Gauss
-    function inv_gauss($esp, $lambda, $x) {
-        if ($x <= 0) {
+function inv_gauss($esp, $lambda, $x) {
+    if ($x <= 0) {
         return 0;
-        }
-        return sqrt($lambda / (2 * pi() * pow($x, 3))) * exp(-$lambda * pow($x - $esp, 2) / (2 * pow($esp, 2) * $x));
-        }
+    }
+    return sqrt($lambda / (2 * pi() * pow($x, 3))) * exp(-$lambda * pow($x - $esp, 2) / (2 * pow($esp, 2) * $x));
+}
 
 ################################## Méthodes de calcul d'intégrales
 
-    function  rectangles_gauche($esp, $lambda, $n, $b) {
-        $proba_fin = [];
-        $intervalle = ($b / $n); // pas besoin de définir a car on commence toujours à 0
-        $h = $b / $n;
-        $proba = 0;
-        for ($i = 1; $i <= $n; $i++) { // Commence à 1 pour éviter la division par zéro
-            $x = $intervalle * $i;
-            $proba += $intervalle * inv_gauss($esp, $lambda, $x);
-            $proba_fin = [$proba]; //On ajoute les valeurs dans une liste pour pouvoir les afficher plus tard
-        }
+#proba_vals --> les valeurs pour la fonction de densité
+#proba_fin --> la valeur du resultat
 
-        return $proba_fin;
+function rectangles_gauche($esp, $lambda, $n, $b) {
+    $proba_vals = [];
+    $proba_fin = 0;
+    $intervalle = $b / $n;
+
+    for ($i = 1; $i <= $n; $i++) {
+        $x = $intervalle * $i;
+        $proba = $intervalle * inv_gauss($esp, $lambda, $x);
+        $proba_vals[] = $proba;
+        $proba_fin += $proba;
     }
 
-    function  rectangles_median($esp, $lambda, $n, $b) {
-        $intervalle = $b / $n;
-        $proba = 0;
-        for ($i = 0; $i < $n; $i++) {
-            $x = ($i + 0.5) * $intervalle;
-            $proba += $intervalle * inv_gauss($esp, $lambda, $x);
-        }
-        return $proba;
+    return [
+        'proba_vals' => $proba_vals,
+        'proba_fin' => $proba_fin
+    ];
+}
+
+
+
+function  rectangles_median($esp, $lambda, $n, $b) {
+    $proba_vals = [];
+    $intervalle = $b / $n;
+    $proba = 0;
+    $proba_fin = 0;
+
+    for ($i = 0; $i < $n; $i++) {
+        $x = ($i + 0.5) * $intervalle;
+        $proba += $intervalle * inv_gauss($esp, $lambda, $x);
+        $proba_vals[] = $proba;
+        $proba_fin += $proba;
+    }
+    return [
+        'proba_vals' => $proba_vals,
+        'proba_fin' => $proba_fin
+    ];
+}
+
+function trapezes($esp, $lambda, $n, $b) {
+    $proba_vals = [];
+    $intervalle = $b / $n;
+    $proba = 0;
+    $proba_fin = 0;
+
+    $proba += 0.5 * inv_gauss($esp, $lambda, 0);
+    $proba += 0.5 * inv_gauss($esp, $lambda, $b);
+
+    for ($i = 1; $i < $n; $i++) {
+        $x = $i * $intervalle;
+        $proba += $intervalle * inv_gauss($esp, $lambda, $x);
+        $proba_vals[] = $proba;
+        $proba_fin += $proba;
     }
 
-    function  trapezes($esp, $lambda, $n, $b) {
-        $intervalle = ($b) / $n;
-        $sum = inv_gauss($esp, $lambda,$b);
-
-        for ($i = 1; $i < $n; $i++) {
-            $x = $i * $intervalle;
-            $sum += 2 * inv_gauss($esp,$lambda,$x);
-        }
-
-        return $intervalle * $sum / 2;
-    }
+    return [
+        'proba_vals' => $proba_vals,
+        'proba_fin' => $proba_fin
+    ];
+}
 
 
 
