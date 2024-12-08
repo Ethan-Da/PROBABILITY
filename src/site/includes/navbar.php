b2895e52ec120ecdadec3d21724cb705ebeaec1c
ArseneMax <arsenemaxencezs@gmail.com>
2024-12-08 19:44:13 +0100

    navbar responsive

--- src/site/includes/navbar.php
+++ src/site/includes/navbar.php
@@ -8,28 +8,35 @@
         <a href="index.php" title="Accueil">
             <img src="../images/logo2.png" alt="Logo" style="height: 80px; margin-right: 20px;">
         </a>
-        <nav>
-            <ul style="list-style-type: none; padding: 0; display: flex;">
-                <li style="margin-right: 20px;">
-                    <a href="index.php" class="bouton-nav" id="bouton-nav0" title="Accueil">Accueil</a></li>
-                <li style="margin-right: 20px;  ">
-                    <a href="module1.php" class="bouton-nav" id="bouton-nav1" title="Module1">Damier</a></li>
-                <li style="margin-right: 20px;">
-                    <a href="module2.php"  class="bouton-nav" id="bouton-nav2" title="Module2">Notes</a></li>
-                <li style="margin-right: 20px;">
-                    <a href="module3.php" class="bouton-nav" id="bouton-nav3" title="Module3">Calcul Proba</a></li>
-                <li style="margin-right: 20px;">
-                    <a href="module4.php" class="bouton-nav" id="bouton-nav4" title="Module4">A venir</a></li>
-                <li style="margin-right: 20px;">
-                    <a href="gestionFiche.php" class="bouton-nav" id="bouton-nav0" title="Fiche de Calcul">Historique</a></li>';
-        if ($_SESSION["login"] == "admin"){
-            echo '<li style="margin-right: 20px;">
-                    <a href="gestionCompte.php" class="bouton-nav" id="bouton-nav0" title="Gestion des comptes">Gestion Comptes</a></li>';
-        }
+        <div class="resp-navbar">
+            <nav>
+                <ul class = "nav-menu" style="list-style-type: none; padding: 0; display: flex;">
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="index.php" class="bouton-nav" id="bouton-nav0" title="Accueil">Accueil</a></li>
+                    <li class="nav-item" style="margin-right: 20px;  ">
+                        <a href="module1.php" class="bouton-nav" id="bouton-nav1" title="Module1">Damier</a></li>
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="module2.php"  class="bouton-nav" id="bouton-nav2" title="Module2">Notes</a></li>
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="module3.php" class="bouton-nav" id="bouton-nav3" title="Module3">Calcul Proba</a></li>
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="module4.php" class="bouton-nav" id="bouton-nav4" title="Module4">A venir</a></li>
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="gestionFiche.php" class="bouton-nav" id="bouton-nav0" title="Fiche de Calcul">Historique</a></li>';
+            if ($_SESSION["login"] == "admin"){
+                echo '<li class="nav-item" style="margin-right: 20px;">
+                        <a href="gestionCompte.php" class="bouton-nav" id="bouton-nav0" title="Gestion des comptes">Gestion Comptes</a></li>';
+            }
 
-        echo '           
-            </ul>   
-        </nav>
+            echo '           
+                </ul>
+                <button class="menu">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </button>   
+            </nav>
+        </div>
     </div>
     <div class="profil-container">
         <button id="button" class="profil-button">Profil</button>
@@ -43,21 +50,28 @@
         <a href="index.php">
             <img src="../images/logo2.png" alt="Logo" style="height: 80px; margin-right: 20px;">
         </a>
-        <nav>
-            <ul style="list-style-type: none; padding: 0; display: flex;">
-                <li style="margin-right: 20px;">
-                    <a href="index.php" class="bouton-nav" id="bouton-nav0">Accueil</a></li>
-                <li style="margin-right: 20px;  ">
-                    <a href="module1.php" class="bouton-nav-disabled">Module1</a></li>
-                <li style="margin-right: 20px;">
-                    <a href="module2.php"  class="bouton-nav-disabled">Module2</a></li>
-                <li style="margin-right: 20px;">
-                    <a href="module3.php" class="bouton-nav-disabled">Module3</a></li>
-                <li style="margin-right: 20px;">
-                    <a href="module4.php" style="" class="bouton-nav-disabled">Module4</a></li>
-            </ul>
-        </nav>
+        <div class="resp-navbar">
+            <nav>
+                <ul class = "nav-menu" style="list-style-type: none; padding: 0; display: flex;">
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="index.php" class="bouton-nav" id="bouton-nav0">Accueil</a></li>
+                    <li class="nav-item" style="margin-right: 20px;  ">
+                        <a href="module1.php" class="bouton-nav-disabled">Module1</a></li>
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="module2.php"  class="bouton-nav-disabled">Module2</a></li>
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="module3.php" class="bouton-nav-disabled">Module3</a></li>
+                    <li class="nav-item" style="margin-right: 20px;">
+                        <a href="module4.php" style="" class="bouton-nav-disabled">Module4</a></li>
+                </ul>
+                    <button class="menu">
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                    </button>
+            </nav>
         </div>
+    </div>
         <div class="profil-container">
             <button id="button" class="profil-button">Profil</button>
         </div> ';
@@ -65,6 +79,28 @@
 
 
     ?>
+    <script>
+document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector(".menu");
+    const navMenu = document.querySelector(".nav-menu");
+
+    menu.addEventListener("click", mobileMenu);
+
+    function mobileMenu() {
+        menu.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+
+    const navLink = document.querySelectorAll(".nav-link");
+
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+    function closeMenu() {
+        menu.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+});
+    </script>
 
 </header>
 
