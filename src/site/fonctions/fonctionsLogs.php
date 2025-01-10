e393d3941b561bff5a0fa36984c829b9c7d45874
Ed <ethan.dangeul@gmail.com>
2025-01-10 10:22:10 +0100

    refactor logs

--- src/site/fonctions/fonctionsLogs.php
+++ src/site/fonctions/fonctionsLogs.php
@@ -2,36 +2,36 @@
 
 
 function logConnec($succes){
-    $login = $_SESSION['login'];
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $date = date("Y-m-d H:i:s");
-    error_log("$ip;$date;$login;Tentative de connexion;$succes",0,"../logs/activites.csv");
+    $fcsv = fopen("../logs/activites.csv", "w");
+    $array = [$_SERVER['REMOTE_ADDR'],date("Y-m-d H:i:s") ,$_SESSION['login'],"Tentative de connexion",$succes];
+    fputcsv($fcsv, $array);
+    fclose($fcsv);
 }
 
 function logSignIn($succes){
-    $login = $_SESSION['login'];
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $date = date("Y-m-d H:i:s");
-    error_log("$ip;$login;Tentative d'inscription;$succes",0,"../logs/activites.csv");
+    $fcsv = fopen("../logs/activites.csv", "w");
+    $array = [$_SERVER['REMOTE_ADDR'],date("Y-m-d H:i:s") ,$_SESSION['login'],"Tentative d'inscription",$succes];
+    fputcsv($fcsv, $array);
+    fclose($fcsv);
 }
 
 function logUseModule($succes, $module){
-    $login = $_SESSION['login'];
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $date = date("Y-m-d H:i:s");
-    error_log("$ip;$login;Utilisation du module $module;$succes",0,"../logs/activites.csv");
+    $fcsv = fopen("../logs/activites.csv", "w");
+    $array = [$_SERVER['REMOTE_ADDR'],date("Y-m-d H:i:s") ,$_SESSION['login'],"Utilisation du module $module",$succes];
+    fputcsv($fcsv, $array);
+    fclose($fcsv);
 }
 
 function logViewHistory($succes){
-    $login = $_SESSION['login'];
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $date = date("Y-m-d H:i:s");
-    error_log("$ip;$login;Utilisation de l'historique;$succes",0,"../logs/activites.csv");
+    $fcsv = fopen("../logs/activites.csv", "w");
+    $array = [$_SERVER['REMOTE_ADDR'],date("Y-m-d H:i:s") ,$_SESSION['login'],"Utilisation de l'historique",$succes];
+    fputcsv($fcsv, $array);
+    fclose($fcsv);
 }
 
 function logDeleteChart($succes){
-    $login = $_SESSION['login'];
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $date = date("Y-m-d H:i:s");
-    error_log("$ip;$login;Suppression d'une fiche de calcul;$succes",0,"../logs/activites.csv");
+    $fcsv = fopen("../logs/activites.csv", "w");
+    $array = [$_SERVER['REMOTE_ADDR'],date("Y-m-d H:i:s") ,$_SESSION['login'],"Suppression d'une fiche de calcul",$succes];
+    fputcsv($fcsv, $array);
+    fclose($fcsv);
 }
