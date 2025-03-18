09f963631c441bcd30b4f21d051743ed738d2140
Romeolbl <leblondromeo@gmail.com>
2025-03-18 20:25:05 +0100

    Page de redirection

--- src/site/pages/actions/saveFichePoly.php
+++ src/site/pages/actions/saveFichePoly.php
@@ -1,1 +1,23 @@
 <?php
+
+require_once "../../fonctions/Database.php";
+
+session_start();
+if (isset($_GET["module"])) {
+    $resultat = $_GET["resultat_str"];
+    $M = $_GET["module"];
+    $a = $_GET["a"];
+    $b = $_GET["b"];
+    $c = $_GET["c"];
+    $login = $_SESSION["login"];
+    $connexionDB = new Database();
+    if ($connexionDB->saveFiche($M, $a, $b, $c, $resultat, $login)) {
+        header("location: ../gestionFiche.php");
+    } else {
+        header("location: ../module4.php");
+    }
+
+
+} else {
+    echo "vous ne venez pas d'un module";
+}
