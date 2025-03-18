e41f2fda2ea6dfdce8ae0102382531576c1250e5
Romeolbl <leblondromeo@gmail.com>
2025-03-18 18:46:49 +0100

    Ajout de fonctions pour calculer les résultat d'une équation polynomiale complexe

--- src/site/fonctions/fonctions_polynome.php
+++ src/site/fonctions/fonctions_polynome.php
@@ -1,1 +1,28 @@
 <?php
+function calculerDiscri($a, $b, $c) {
+    return ($b * $b) - (4 * $a * $c);
+}
+
+function calculerRacinesR($a, $b, $delta) {
+    $x1 = (-$b + sqrt($delta)) / (2 * $a);
+    $x2 = (-$b - sqrt($delta)) / (2 * $a);
+
+    return [$x1,$x2];
+}
+function calculerRacineUnique($a, $b) {
+    return -$b / (2 * $a) ;
+}
+function calculerRacinesCmp($a, $b, $delta) {
+    $partieR = -$b / (2 * $a);
+    $partieIm = sqrt(abs($delta)) / (2 * $a);
+
+    // Construction des chaînes représentant les nombres complexes
+    $z1 = $partieR . ' + ' . $partieIm . 'i';
+    $z2 = $partieR . ' - ' . $partieIm . 'i';
+
+    return [
+        'z1' => $z1,
+        'z2' => $z2
+    ];
+}
+?>
