52e487e5efb37643c199f3138831be87aef243e0
Romeolbl <leblondromeo@gmail.com>
2025-03-18 18:43:02 +0100

    Ajout d'une page de calcul qui stocke les résultats et le renvoie dans le module

---
+++ src/site/pages/actions/calcul_polynome.php
@@ -0,0 +1,33 @@
+<?php
+include '../../includes/header.php';
+require_once '../../fonctions/fonctions_polynome.php';
+require_once '../../fonctions/fonctionsDroits.php';
+include '../../includes/profil.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $a = $_POST['coef_a'];
+    $b = $_POST['coef_b'];
+    $c = $_POST['coef_c'];
+
+    //erreur lors de a = 0
+    if ($a == 0) {
+        header("Location: module4.php?error=Le coefficient a ne peut pas être égal à 0 pour une équation du second degré");
+        exit();
+    }
+
+    $delta = calculerDiscri($a, $b, $c);
+
+    if ($delta > 0) {
+        list($x1, $x2) = calculerRacinesR($a, $b, $delta);
+        header("Location: ../module4.php?delta=$delta&x1=$x1&x2=$x2");
+    }
+    elseif ($delta == 0) {
+        $x = calculerRacineUnique($a, $b);
+        header("Location: ../module4.php?delta=$delta&x=$x");
+    }
+    else {
+        $racinesCmp = calculerRacinesCmp($a, $b, $delta);
+        header("Location: ../module4.php?delta=$delta&z1={$racinesCmp['z1']}&z2={$racinesCmp['z2']}");
+    }
+}
+?>
