5e2ae589b86c94d901b2d4beaccf690a3f4abf74
Romeo Leblond <22301119@me.uvsq.fr>
2025-04-03 17:11:37 +0200

    modif fct logs

--- src/site/fonctions/fonctionsLogs.php
+++ src/site/fonctions/fonctionsLogs.php
@@ -1,24 +1,25 @@
 <?php
 
 
-function logConnec($succes, $login){
+function logConnec($succes)
+{
+    function logConnec($succes, $login)
+    {
 
-    $date = getdate();
-    $fjson = file_get_contents("/home/sitesae/src/site/logs/log-" . "$date[mday]" . "-$date[mon]" . "-$date[year]" . ".json", "a+");
-    $json = json_decode($fjson, true);
+        $date = getdate();
+        $fjson = file_get_contents("../logs/log-" . "$date[mday]" . "-$date[mon]" . "-$date[year]" . ".json", "a+");
+        $json = json_decode($fjson, true);
 
-    $dict = array("date"=>date("d-m-Y"),
-                    "ip" => $_SERVER['REMOTE_ADDR'],
-                    "login" => $login,
-                    "success"=>$succes);
+        $dict = array("date" => date("d-m-Y"),
+            "ip" => $_SERVER['REMOTE_ADDR'],
+            "login" => $_SESSION['login'],
+            "login" => $login,
+            "success" => $succes);
 
-    $json['logs'][date("H:i:s")] = $dict;
+        $json['logs'][date("H:i:s")] = $dict;
 
 
-    $json = json_encode($json, JSON_PRETTY_PRINT);
-    file_put_contents("/home/sitesae/src/site/logs/log-"."$date[mday]"."-$date[mon]"."-$date[year]".".json", $json);
+        $json = json_encode($json, JSON_PRETTY_PRINT);
+        file_put_contents("../logs/log-" . "$date[mday]" . "-$date[mon]" . "-$date[year]" . ".json", $json);
+    }
 }
-
-
-
-
