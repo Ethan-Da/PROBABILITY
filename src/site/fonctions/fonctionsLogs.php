df172767d9aa91badccd844992732b432e5b9983
Romeo Leblond <22301119@me.uvsq.fr>
2025-04-03 16:09:51 +0200

    logs

--- src/site/fonctions/fonctionsLogs.php
+++ src/site/fonctions/fonctionsLogs.php
@@ -4,7 +4,7 @@
 function logConnec($succes, $login){
 
     $date = getdate();
-    $fjson = file_get_contents("../logs/log-" . "$date[mday]" . "-$date[mon]" . "-$date[year]" . ".json", "a+");
+    $fjson = file_get_contents("/home/sitesae/src/site/logs/log-" . "$date[mday]" . "-$date[mon]" . "-$date[year]" . ".json", "a+");
     $json = json_decode($fjson, true);
 
     $dict = array("date"=>date("d-m-Y"),
@@ -16,7 +16,7 @@ function logConnec($succes, $login){
 
 
     $json = json_encode($json, JSON_PRETTY_PRINT);
-    file_put_contents("../logs/log-"."$date[mday]"."-$date[mon]"."-$date[year]".".json", $json);
+    file_put_contents("/home/sitesae/src/site/logs/log-"."$date[mday]"."-$date[mon]"."-$date[year]".".json", $json);
 }
 
 
